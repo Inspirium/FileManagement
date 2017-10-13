8f813227461afe15851f23c690caf72951877769
Marko Banusic <mbanusic@gmail.com>
2017-10-14 01:22:52 +0200

    Docs for models

--- src/Models/Document.php
+++ src/Models/Document.php
@@ -5,6 +5,12 @@ namespace Inspirium\FileManagement\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * Inspirium\FileManagement\Models\Document
+ *
+ * @property-read \Inspirium\HumanResources\Models\Employee $owner
+ * @mixin \Eloquent
+ */
 class Document extends File {
 	public $type = 'document';
 }
--- src/Models/File.php
+++ src/Models/File.php
@@ -6,15 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
- * Class File
- * @package Inspirium\FileManagement\Models
+ * Inspirium\FileManagement\Models\File
  *
- * @property $id
- * @property $title
- * @property $location
- * @property $link
- * @property $type
- * @property $owner_id
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $location
+ * @property string|null $link
+ * @property string|null $type
+ * @property int|null $owner_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \Inspirium\HumanResources\Models\Employee|null $owner
+ * @method static bool|null forceDelete()
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\FileManagement\Models\File onlyTrashed()
+ * @method static bool|null restore()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereOwnerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\FileManagement\Models\File whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\FileManagement\Models\File withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\FileManagement\Models\File withoutTrashed()
+ * @mixin \Eloquent
  */
 class File extends Model {
 
